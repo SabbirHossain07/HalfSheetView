b95bedc424a7759217914ccf53c36652278c92b8
SabbirHossain07 <sabbirsopnil07@gmail.com>
2021-12-25 00:35:26 +0600

    Initial Commit

---
+++ SheetView/SheetViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetViewApp.swift
+//  SheetView
+//
+//  Created by Sopnil Sohan on 25/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SheetViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
